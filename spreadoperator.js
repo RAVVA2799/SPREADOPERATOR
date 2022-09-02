62392f32365dcc5434bd5fc571c2538980f59cb7
RAVVA2799 <80970669+RAVVA2799@users.noreply.github.com>
2022-09-02 11:58:49 +0530

    Add files via upload

---
+++ spreadoperator.js
@@ -0,0 +1,15 @@
+
+//sprrad operator//
+
+const numbers1=[1,2,3,4,5];
+const numbers2=[6,7,8,9,10];
+const numbers3Combined=[...numbers1,...numbers2];  //combines both the array
+
+console.log(numbers3Combined);
+
+
+const n1=[1,2,3,4,8];
+const n2=[6,7,8,9,20];
+const numbersConcat=[...n1,...n2];  //combines both the array
+
+console.log(numbersConcat);
